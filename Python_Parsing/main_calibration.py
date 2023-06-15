f3f467e1c97f0232b0c734ca116612d1ba40dc22
kfoysalhaque <haque.k@northeastern.edu>
2023-06-15 18:25:50 -0400

    update

--- Python_Parsing/main_calibration.py
+++ Python_Parsing/main_calibration.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import os
 import glob
 
-directory = '../Data1/Captures_315/'
-directory_sync = '../Data1/Captures_315/Synced/'
+array_names =['A01', 'A02', 'A03', 'A04', 'A05', 'A06', 'A07', 'A08', 'A09', 'B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B09']
+
+directory = '../Data1/Captures_calibration/'
 extension = '*.csi'  # Replace with the desired file extension
 
 # Create the search pattern
@@ -16,16 +17,14 @@ files = glob.glob(search_pattern)
 
 for file in files:
     seq_array = file[-7:-4]
-    synced_file = directory + file[-7:-4] + ".npy"
-    saved_file = directory_sync + file[-7:-4] + ".npy"
-    S = np.load(synced_file)
 
     globals()[seq_array]=[]
+    A = []
 
     frames = Picoscenes(file)
-    for i in S:
+    for i in range(1000):
+        saved_file = directory + 'Synced/' + file[-7:-4] + ".npy"
         numTones = frames.raw[i].get("CSI").get("numTones")
         CSI = np.array(frames.raw[i].get("CSI").get("CSI"))[:numTones]
-
         np.array(globals()[seq_array].append(CSI))
     np.save(saved_file, globals()[seq_array])
